206b5f2fc12bbb0fe4009a98da5cf7105d2b870c
Rinkels <61621485+Rinkels@users.noreply.github.com>
2026-08-10 12:11:08 -0600

    backup: R1-UFO 2026-08-10 12:11

---
+++ apps/aegis/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class AegisConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.aegis"
+    label = "aegis"
+    verbose_name = "Aegis"
